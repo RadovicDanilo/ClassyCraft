def6f5e2733580e31c5b0413cbc013989ba351cf
Danilo <daniloradovic2003@gmail.com>
2023-10-18 13:32:43 +0200

    kod od utorka 17.10

--- src/main/java/raf/dsw/classycraft/app/gui/swing/controller/AboutUsAction.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/controller/AboutUsAction.java
@@ -1,13 +1,14 @@
 package main.java.raf.dsw.classycraft.app.gui.swing.controller;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 public class AboutUsAction extends AbstractAction {
 
     public AboutUsAction() {
-        putValue(SMALL_ICON, null); //TODO [LOW] dodaj ikonu za about us
+        putValue(SMALL_ICON, null); //TODO dodaj ikonu za about us
         putValue(NAME, "About us");
         putValue(SHORT_DESCRIPTION, "About us");
     }
